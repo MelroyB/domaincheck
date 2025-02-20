0336a4528a44f2a816c6494a5d261a8f71733901
Melroy <39052594+MelroyB@users.noreply.github.com>
2025-02-20 11:04:20 +0100

    Create start.sh

---
+++ docker/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Controleer of de REPO_URL variabele is ingesteld
+if [ -z "$REPO_URL" ]; then
+  echo "REPO_URL environment variable is not set. Exiting."
+  exit 1
+fi
+
+# Clone de repository
+git clone $REPO_URL repo
+
+# Ga naar de directory van de repository
+cd repo
+
+# Installeer Python dependencies
+pip install -r requirements.txt
+
+# Start de Flask applicatie
+python app.py
+
+
